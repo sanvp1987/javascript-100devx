c93d42cbdfbca77ed1d8c38ca8ddf949fe2ce7d9
sanvp1987 <68700853+sanvp1987@users.noreply.github.com>
2024-08-11 13:51:24 +0000

    sumall added

--- functions.js
+++ functions.js
@@ -1,10 +1,19 @@
+const sumall=(a,b) => console.log(parseInt(a)+b);
+sumall("2",3)
+
 const sum=(n)=>{
     let ans = 0
     for(let i = 0; i <= n; i++){
         ans= ans + i;
     }
-    //return n *(n+1);
     return ans;
+    // above Loop Code or below Line Only
+      //return n *(n+1); //
 }
 
-console.log(sum(10))
+const ons1 = sum(10)
+console.log(ons1)
+const ons2 = sum(100)
+console.log(ons2)
+const ons3 = sum(1000)
+console.log(ons3) 
