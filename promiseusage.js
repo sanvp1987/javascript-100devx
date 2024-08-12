4a057de473b82d0ad803212a4b1dffeb91fce480
sanvp1987 <68700853+sanvp1987@users.noreply.github.com>
2024-08-12 15:33:22 +0000

    Promise Usage

--- promiseusage.js
+++ promiseusage.js
@@ -21,23 +21,40 @@
 
 // ****************** Promise with Fetch *********************
 
-const fetchData = (url)=>{
-    console.log(`url : ${url}`)
-    return fetch(url).then(response =>{
-        if(!response.ok){
-            throw new Error(`HTTP error! Status: ${response.status}`)
-        }
-        return response.json()
-    })
+// const fetchData = (url)=>{
+//     console.log(`url : ${url}`)
+//     return fetch(url).then(response =>{
+//         if(!response.ok){
+//             throw new Error(`HTTP error! Status: ${response.status}`)
+//         }
+//         return response.json()
+//     })
+// }
+
+// const run = async ()=>{
+//     console.log("Fetching Data...")
+//     const data = await fetchData("https://jsonplaceholder.typicode.com/todos/1");
+//     console.log(data)
+//     console.log("Fetching Data Done.")
+// }
+
+// run()
+// ****************** Promise with Fetch *********************
+
+// ****************** Promise with Fs.ReadFile *********************
+const fs = require("fs").promises
+
+const readFile = async (path) =>{
+    const contents = fs.readFile(path,"utf-8")
+    return contents;
 }
 
 const run = async ()=>{
-    console.log("Fetching Data...")
-    const data = await fetchData("https://jsonplaceholder.typicode.com/todos/1");
-    console.log(data)
-    console.log("Fetching Data Done.")
+    console.log("Reading File...")
+    const contents = await readFile("a.txt")
+    console.log(`File Contents : ${contents}`)
+    console.log("File Read Done")
 }
 
 run()
-
-// ****************** Promise with Fetch *********************
+// ****************** Promise with Fs.ReadFile *********************
