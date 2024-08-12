6c7805ffc351eb69ef8bddf937182edd1b14e6d5
sanvp1987 <68700853+sanvp1987@users.noreply.github.com>
2024-08-12 14:49:26 +0000

    event loop added

--- eventloop.js
+++ eventloop.js
@@ -3,4 +3,14 @@ const timeout=()=>{console.log("Click the button")}
 console.log("Hi")
 setTimeout(timeout, 1000);
 console.log("Welcome to Loupe.")
+ 
+let c = 0
+for (let i = 0; i < 10000000000; i++) {
+    c = c + 1;
+}
+console.log("Expensive Operation Done ! ",c)
 
+// Hi
+// Welcome to Loupe.
+// Expensive Operation Done !  10000000000
+// Click the button
